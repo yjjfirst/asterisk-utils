b0da3852aa1294f19241f527804bd95079c79210
Martin.Yang <yjjfirst@hotmail.com>
2017-11-22 14:29:00 +0800

    Add hotdesk dialplan

---
+++ hotdesk/hot-desk.sql
@@ -0,0 +1,16 @@
+INSERT INTO ast_hotdesk (extension, first_name,
+last_name, cid_name, cid_number, pin, context)
+VALUES (1104,'Matt','Jordan','Matt Jordan',
+'4165551105','559','international');
+
+INSERT INTO ast_hotdesk (extension, first_name,
+last_name, cid_name, cid_number, pin, context)
+VALUES (1102,'Jim','Van Meggelen','Jim Van Meggelen',
+'4165551102','561','international');
+
+INSERT INTO ast_hotdesk (extension, first_name,
+last_name, cid_name, cid_number, pin, context)
+VALUES (1103,'Russell','Bryant','Russell Bryant',
+'4165551103','563','international');
+
+
